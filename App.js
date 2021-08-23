a6519ee9dd57c6a43157ed225fd79d4fcde420b6
mehmood-gap <mehmoodmunir@gap-dynamics.com>
2021-08-23 15:29:15 +0500

    1 test simple is running

--- App.js
+++ App.js
@@ -18,64 +18,71 @@ import {
 } from 'react-native';
 
 
-import LoginScreen from './src/Screens/LoginScreen';
+// import LoginScreen from './src/Screens/LoginScreen';
 
-import { NavigationContainer } from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+// import { NavigationContainer } from '@react-navigation/native';
+// import {createStackNavigator} from '@react-navigation/stack';
 
-const Stack = createStackNavigator();
+// const Stack = createStackNavigator();
 
-const Auth = () => {
-  // Stack Navigator for Login and Sign up Screen
-  return (
-    <Stack.Navigator initialRouteName="LoginScreen">
-      <Stack.Screen
-        name="LoginScreen"
-        component={LoginScreen}
-        options={{headerShown: false}}
-      />
-      {/* <Stack.Screen
-        name="ForgotScreen"
-        component={ForgotScreen}
-        options={{headerShown: false}}
-      /> */}
-    </Stack.Navigator>
-  );
-};
+// const Auth = () => {
+//   // Stack Navigator for Login and Sign up Screen
+//   return (
+//     <Stack.Navigator initialRouteName="LoginScreen">
+//       <Stack.Screen
+//         name="LoginScreen"
+//         component={LoginScreen}
+//         options={{headerShown: false}}
+//       />
+//       {/* <Stack.Screen
+//         name="ForgotScreen"
+//         component={ForgotScreen}
+//         options={{headerShown: false}}
+//       /> */}
+//     </Stack.Navigator>
+//   );
+// };
 const App = () => {
 
   return (
 
+    <SafeAreaView>
+      <ScrollView>
+        <View>
+          <Text>Hello World</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
     
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Auth">
-        {/* SplashScreen which will come once for 2 Seconds */}
-        {/* <Stack.Screen
-          name="Splash"
-          component={Splash}
-          // Hiding header for Splash Screen
-          options={{headerShown: false}}
-        /> */}
-        {/* Auth Navigator: Include Login and Signup */}
-        <Stack.Screen
-          name="Auth"
-          component={Auth}
-          options={{headerShown: false}}
-        />
-        {/* <Stack.Screen
-          name="Main"
-          component={Main}
-          options={{headerShown: false}}
-        /> */}
-        {/* Navigation Drawer as a landing page
-        {/* <Stack.Screen
-          name="DrawerNavigationRoutes"
-          component={DrawerNavigationRoutes}
-          // Hiding header for Navigation Drawer
-          options={{headerShown: false}}
-        /> */}
-      </Stack.Navigator>
-    </NavigationContainer>
+    // <NavigationContainer>
+    //   <Stack.Navigator initialRouteName="Auth">
+    //     {/* SplashScreen which will come once for 2 Seconds */}
+    //     {/* <Stack.Screen
+    //       name="Splash"
+    //       component={Splash}
+    //       // Hiding header for Splash Screen
+    //       options={{headerShown: false}}
+    //     /> */}
+    //     {/* Auth Navigator: Include Login and Signup */}
+    //     <Stack.Screen
+    //       name="Auth"
+    //       component={Auth}
+    //       options={{headerShown: false}}
+    //     />
+    //     {/* <Stack.Screen
+    //       name="Main"
+    //       component={Main}
+    //       options={{headerShown: false}}
+    //     /> */}
+    //     {/* Navigation Drawer as a landing page
+    //     {/* <Stack.Screen
+    //       name="DrawerNavigationRoutes"
+    //       component={DrawerNavigationRoutes}
+    //       // Hiding header for Navigation Drawer
+    //       options={{headerShown: false}}
+    //     /> */}
+    //   </Stack.Navigator>
+    // </NavigationContainer>
     
   );
 };
